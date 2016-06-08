15b5caa9ca170b179fb548a152a62db6f28d957c
Komal Sharma <komal@imaginei.in>
2016-06-08 15:58:05 +0530

    Interface for syncing: New

---
+++ app/src/main/java/com/moblyo/market/interfaces/SortCouponsByDistanceCallback.java
@@ -0,0 +1,8 @@
+package com.moblyo.market.interfaces;
+
+/**
+ * Created by Komal on 07/05/16.
+ */
+public interface SortCouponsByDistanceCallback {
+    public void refreshData(boolean refresh);
+}
